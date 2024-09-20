fe37f291ef139a931abac937fa1b632d4c11b37d
John Levermore <john.levermore@zhibek.com>
2024-09-20 10:43:23 +0100

    feat: Improve StationSelect logic

--- src/StationSelect.tsx
+++ src/StationSelect.tsx
@@ -1,22 +1,65 @@
+import { useState } from 'react';
+
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/FormLabel';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
+import { debounce } from '@mui/material/utils';
 
-import stationsData from './data/stations.json';
+import stationsDataJson from './data/stations.json';
 
-if (stationsData?.length <= 1) {
+if (stationsDataJson?.length <= 1) {
   console.warn('Stations data needs to be generated using "npm run generate-data"!');
 }
 
+interface StationOption {
+  id: string,
+  name: string,
+}
+
+const stationsData: StationOption[] = stationsDataJson;
+
 function StationSelect({ id, label, setValue = () => {} }: { id: string, label: string, setValue?: (newValue: string | null) => void }) {
+  const [options, setOptions] = useState<StationOption[]>([]);
+
+  const buildOptions = (searchTerm: string): StationOption[] => {
+    const diatrictsRegex = /[\u0300-\u036f]/g;
+
+    const searchTermList = searchTerm.split(' ');
+    const searchRegexList = searchTermList.map((item) => (
+      item.trim() ? new RegExp(item.trim(), 'i') : null
+    )).filter((item) => (item));
+
+    const stationOptions = stationsData.filter((station) => {
+      const stationNameNormalised = station.name.normalize('NFD').replace(diatrictsRegex, '');
+
+      return searchRegexList.every((searchRegex) => (
+        searchRegex && stationNameNormalised.match(searchRegex)
+      ));
+    });
+
+    return stationOptions;
+  };
+
+  const onInputChange = (_: unknown, value: string) => {
+    if (!value || value.length < 3) {
+      setOptions([]);
+      return;
+    }
+
+    const newOptions = buildOptions(value);
+    setOptions(newOptions);
+  };
+
   return (
     <FormControl fullWidth>
 
       <InputLabel htmlFor={id}>{label}</InputLabel>
 
       <Autocomplete
-        options={stationsData}
+        options={options}
+        onInputChange={debounce(onInputChange, 100)}
+        filterOptions={(x) => x}
         renderInput={(params) => (
           <TextField {...params} id={id} variant="filled" placeholder="Select station..." />
         )}
